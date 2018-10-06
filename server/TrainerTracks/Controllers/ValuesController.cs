0a8fdc04e4df688280151966c5c89e2c3241446c
watkins0049 <nick.watkins49@gmail.com>
2018-10-06 16:58:31 -0400

    Initial project setup.

---
+++ server/TrainerTracks/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TrainerTracks.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        [HttpGet("HelloWorld")]
+        public IEnumerable<string> HelloWorld()
+        {
+            return new string[] { "Hello World!" };
+        }
+    }
+}
