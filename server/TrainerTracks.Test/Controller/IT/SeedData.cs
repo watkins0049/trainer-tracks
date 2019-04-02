f43f815c596547133b1e457c0f04fac2a9325f69
Nick Watkins <nick.watkins49@gmail.com>
2019-04-02 16:09:32 -0400

    Removed controller unit tests since those didn't make much sense.
    Added integration tests.

---
+++ server/TrainerTracks.Test/Controller/IT/SeedData.cs
@@ -0,0 +1,27 @@
+﻿using TrainerTracks.Data.Context;
+using TrainerTracks.Data.Model.Entity;
+
+namespace TrainerTracks.Web.Test.Controller.IT
+{
+    public static class SeedData
+    {
+        public static void PopulateTestData(AccountContext dbContext)
+        {
+            dbContext.Trainer.Add(new Trainer
+            {
+                TrainerId = 1,
+                EmailAddress = "nick.watkins49@gmail.com",
+                FirstName = "Nick",
+                LastName = "Watkins"
+            });
+            dbContext.TrainerCredentials.Add(new TrainerCredentials
+            {
+                TrainerId = 1,
+                // the hash of the SHA512 hash of "password1234"
+                Hash = "$2b$10$sCfS.t4SiS21G9rhNcqKuemSpI8sU/F6z59x.aZimKouY2qLFp69.",
+                Salt = "$2b$10$sCfS.t4SiS21G9rhNcqKue"
+            });
+            dbContext.SaveChanges();
+        }
+    }
+}
---
+++ server/TrainerTracks.Test/Controller/IT/UserClaimsDtoJsonConverter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using TrainerTracks.Data.Model.DTO.Account;
+
+namespace TrainerTracks.Web.Test.Controller.IT
+{
+    public class UserClaimsDtoJsonConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return (objectType == typeof(UserClaimsDTO));
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            JObject jo = JObject.Load(reader);
+
+            JToken claimTokens = jo["claims"];
+            List<Claim> claims = new List<Claim>();
+            foreach(JToken token in claimTokens)
+            {
+                string type = (string)token["type"];
+                string value = (string)token["value"];
+                string valueType = (string)token["valueType"];
+                string issuer = (string)token["issuer"];
+                string originalIssuer = (string)token["originalIssuer"];
+                Claim claim = new Claim(type, value, valueType, issuer, originalIssuer);
+                claims.Add(claim);
+            }
+
+            return new UserClaimsDTO
+            {
+                Claims = claims,
+                Token = (string)jo["token"]
+            };
+        }
+
+        public override bool CanWrite
+        {
+            get { return false; }
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
